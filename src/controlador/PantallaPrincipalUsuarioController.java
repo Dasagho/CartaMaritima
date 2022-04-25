a86b82de1b3990988d039bad6d958b4cc15a00fe
Paulus Galasci <pablo02hg@gmail.com>
2022-04-25 18:38:38 +0200

    Avances en PantallaPrincipalDeUsuario

--- src/controlador/PantallaPrincipalUsuarioController.java
+++ src/controlador/PantallaPrincipalUsuarioController.java
@@ -8,6 +8,8 @@ package controlador;
 import static aplicacion.Main.setRoot;
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -15,10 +17,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import model.Session;
 
 /**
  * FXML Controller class
@@ -29,6 +37,14 @@ public class PantallaPrincipalUsuarioController implements Initializable {
 
     @FXML
     private Label etiquetaAlias;
+    @FXML
+    private ImageView imagenPerfil;
+    @FXML
+    private Label xRealizados;
+    @FXML
+    private Label xAcertados;
+    @FXML
+    private Label xPorcentaje;
 
 
     @Override
@@ -36,6 +52,25 @@ public class PantallaPrincipalUsuarioController implements Initializable {
         // Esto se ejecuta al iniciar
         modelo.secretario.setTitulo("Menú principal");
         etiquetaAlias.setText(modelo.secretario.getUsuario().getNickName());
+        imagenPerfil.setImage(modelo.secretario.getUsuario().getAvatar());
+        
+        // Resumen de historial
+        List<Session> sesiones = modelo.secretario.getUsuario().getSessions();
+        int problemasRealizados = 0;
+        int aciertos = 0;
+        Float porcentaje;
+        for (int i = 0; i < sesiones.size(); i++){
+            aciertos += sesiones.get(i).getHits();
+            problemasRealizados += (aciertos + sesiones.get(i).getFaults());
+        }
+        xRealizados.setText("" + problemasRealizados);
+        xAcertados.setText("" + aciertos);
+        
+        porcentaje = ((float) aciertos) / ((float) problemasRealizados);
+        if (porcentaje.toString() != "NaN"){
+            xPorcentaje.setText("" + porcentaje + "%");
+        } else {xPorcentaje.setText("¿?");}
+        
     }
     /**
      * Para el initialize: - Recupera el objeto sesion y el objeto usuario del
@@ -43,21 +78,25 @@ public class PantallaPrincipalUsuarioController implements Initializable {
      */
     
     
-    
-    /**
-     * Enseña el mapa? seguro?
-     */
-    public void ensenaMapa(ActionEvent e) {}
-
-    
     /**
      * Crea un dialogo modal confirmando que se quiere cerrar sesion y cambia la
      * vista a la inicio de sesion
      */
     @FXML
     private void pulsarCerrarSesion(ActionEvent event) throws IOException {
-        modelo.secretario.cerrarSesion();
-        setRoot("inicioSesion");
+        
+        Alert alerta = new Alert(AlertType.CONFIRMATION);
+        alerta.setTitle("Cerrar sesión");
+        alerta.setHeaderText("Cerrar sesión");
+        alerta.setContentText("¿Seguro que quieres cerrar la sesión?\n ");
+        Optional<ButtonType> respuesta = alerta.showAndWait();
+        if (respuesta.isPresent() && respuesta.get()==ButtonType.OK){
+            // Se cierra la sesión
+            modelo.secretario.cerrarSesion();
+            setRoot("inicioSesion");
+        }
+        
+        
     }
 
     
@@ -75,7 +114,8 @@ public class PantallaPrincipalUsuarioController implements Initializable {
      * vista a la de ver resultados
      */
     @FXML
-    private void pulsarVerResultados(ActionEvent event) {
+    private void pulsarVerResultados(ActionEvent event) throws IOException {
+        setRoot("ResultadosUsuario");
     }
     
     
@@ -98,12 +138,31 @@ public class PantallaPrincipalUsuarioController implements Initializable {
         Scene escena = new Scene(root, 600, 400);
         Stage escenario = new Stage();
         escenario.setScene(escena);
-     //   escenario.setTitle("Editando tu perfil");
         escenario.initModality(Modality.APPLICATION_MODAL); // Hacemos que la ventana nueva sea modal
         escenario.showAndWait();
     
         
     }
+
+    @FXML
+    private void pulsarMapa(MouseEvent event) throws IOException {
+        FXMLLoader miCargador = new FXMLLoader(getClass().getResource("/vista/cartaNavegacion.fxml"));
+        Parent root = miCargador.load();
+        
+        
+        
+        Scene escena = new Scene(root, 900, 600);
+        Stage escenario = new Stage();
+        escenario.setScene(escena);
+     //   escenario.setTitle("Editando tu perfil");
+     //   escenario.initModality(Modality.APPLICATION_MODAL); // Hacemos que la ventana nueva sea modal
+        escenario.show();
+    }
+
+    @FXML
+    private void pulsar(ActionEvent event) {
+    }
+
         
 
 
