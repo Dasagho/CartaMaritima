e66c58f32751fb84525e5ce0f376a93ea11e57c0
david <davidamir03@gmail.com>
2022-05-02 22:34:29 +0200

    Limpiado errores del merge
    Despues  de hacer Merge con la rama de RegisXmodPerfil para incorporar los cambios a mi rama y borrar la rama sobrante

--- src/modelo/secretario.java
+++ src/modelo/secretario.java
@@ -66,11 +66,6 @@ public class secretario {
 // - - - - - - - - - - - - - - - - - - - - - - - - 
 
     
-    
-    
-    public static List<Problem> getProblemas() {
-        return lista;
-    }
 
     /**
      * Objeto de Navegacion intermediaria entre la DB y nuestros controladores
@@ -108,39 +103,4 @@ public class secretario {
     public static void setTitulo(String titulo) {
         aplicacion.Main.getStage().setTitle("Subnautica" + " - " + titulo);
     }
-
-    /**
-     * Objeto de Navegacion intermediaria entre la DB y nuestros controladores
-     *
-     * @return nav
-     */
-    public static Navegacion getNavegacion() {
-        return nav;
-    }
-
-    /**
-     * Metodo inicializador del Singleton capaz de comunicarse con la base de
-     * datos y proveer sus metodos
-     */
-    public static void initialize() {
-        try {
-            nav = getSingletonNavegacion();
-
-        } catch (NavegacionDAOException ex) {
-            System.out.println("No ha sido posible conectarse a la base de datos");
-            ex.printStackTrace();
-        }
-        lista = nav.getProblems();
-        
-    }
-
-    // Modificar para que el titulo quede algo como: Subnautica - Iniciar sesion
-    // modelo.secretario.setTitulo("");
-    /**
-     * format(param) -> aplicacion.Main.getStage().setTitle(param)
-     */
-    public static void setTitulo(String titulo) {
-        aplicacion.Main.getStage().setTitle("Subnautica" + " - " + titulo);
-    }
-
 }
